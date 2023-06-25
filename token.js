177dc288b1292e150e402cb51c016b04813aed6c
germainelee02 <97350904+germainelee02@users.noreply.github.com>
2023-06-25 11:21:55 +0800

    Delete token.js

--- token.js
+++
@@ -1,5 +0,0 @@
-const { Telegraf } = require("telegraf");
-const bot = new Telegraf("5576426748:AAGLIoktXrDmKKjQtMx2A_WyemWI68K0cLM");
-const key = "AIzaSyAvJ7fWhCOIrXwnQvmGyLAs3dzMdQNDo7g";
-
-module.exports = { bot, key };
